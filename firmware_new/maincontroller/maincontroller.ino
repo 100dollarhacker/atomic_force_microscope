98a3e78bbf3170e5892fe63e6c619e43112301ab
Eduard Sinelnikov <100dollarhacker@gmail.com>
2024-08-16 21:48:21 -0400

    new functions and samples

--- firmware_new/maincontroller/maincontroller.ino
+++ firmware_new/maincontroller/maincontroller.ino
@@ -10,23 +10,35 @@
 #define CLK   6	///< SPI Clock pin number
 #define FSYNC 7	///< SPI Load pin number (FSYNC in AD9833 usage)
 
-// #define LDAC 9 //<- for nano  //7 //ldac HIGH to stop DAC output while clocking. 
+#define LDAC 9 //<- for nano  //7 //ldac HIGH to stop DAC output while clocking. 
 
 
-#define TEST_NUM 3
+#define TEST_NUM 6
 
 MD_AD9833	AD(DATA, CLK, FSYNC); // Arbitrary SPI pins
 
 int pincontact = 8;
 int pincontact5v = 9;
 int contact;
-const float BASE_FREQ = 32751.3;
+const float BASE_FREQ = 31262;//32750.3;
 float freq = BASE_FREQ;
 ///////////////////// FreqSensor end ///////////////////////////
 
 //Communication parameters
 #define BAUDRATE 9600//250000   //Serial interfaces communication speed (bps)
 
+struct freq_resp
+{
+  int result;
+  float freq;
+};
+
+struct XYZ_t 
+{
+    int x;
+    int y; 
+    int z; 
+};
 
 RTx* rtx = new RTx();
 
@@ -43,24 +55,50 @@ public:
     AD.setFrequency(0, freq); //TODO::ES - do we need this?
   }
 
-  void GetFreq()
+  void GetFreqRange()
   {
-      for (int i = 0 ; i < 30 ; i++)
+      float delta=0;
+      for (int i = 0 ; i < 50 ; i++)
       {
-        delay(500);
-        GetFreqOnce();
+          freq = BASE_FREQ - 20 + i ;
+          freq_resp res = GetFreqOnce(delta, BASE_FREQ-20, BASE_FREQ + 20);
       }
-
   }
 
-  void GetFreqOnce()
+  freq_resp GetFreq()
   {
-    freq += 0.1;
+      freq_resp min_res;
+      min_res.result = 9999;
+      freq = BASE_FREQ;
+      float delta = 0.1;
+      for (int i = 0 ; i < 15 ; i++)
+      {
+        // delay(5);
+        freq_resp res = GetFreqOnce(delta, BASE_FREQ, BASE_FREQ + 3);
+        if (res.result < min_res.result)
+           min_res = res;
+      }
+
+      Serial.print("min avg  ");
+      Serial.print(min_res.result);
+
+      // Serial.print(x);
+      Serial.print("  @ freq: ");
+      Serial.println(min_res.freq);
+
+      return min_res;
+
+  }
+private:
+  freq_resp GetFreqOnce(float delta, float base_freq, float max_freq)
+  {
+    freq += delta;
+    freq_resp fres;
 
     AD.setFrequency(0, freq);
     
-    if (freq > BASE_FREQ  + 3)
-    freq = BASE_FREQ ; //-7;
+    if (freq > max_freq)
+       freq = base_freq ; //-7;
     avg = 1;
     // for (count =0, result = 0 ; count < 65535; count ++) 
     for (int test = 0; test < TEST_NUM ; test++) {
@@ -88,8 +126,14 @@ public:
     // Serial.print(x);
     Serial.print("  @ freq: ");
     Serial.println(freq);
+
+    fres.freq = freq;
+    fres.result = result;
+    
+    return fres;
+
   }
-private: 
+
   uint16_t count ;
   uint16_t result ;
   uint16_t x;
@@ -104,22 +148,18 @@ public:
   DAC() {
     }
 
-  void X_left( uint16_t posttion ){ad5696->SetVoltage(X_LEFT_CHANNEL, ZERO_POS + posttion);}
-  void X_right( uint16_t posttion ){ad5696->SetVoltage(X_RIGHT_CHANNEL, ZERO_POS + posttion);}
+  void X_left( uint16_t posttion ){ad5696->SetVoltage(X_LEFT_CHANNEL,  posttion);}
+  void X_right( uint16_t posttion ){ad5696->SetVoltage(X_RIGHT_CHANNEL,  posttion);}
     
-  void Y_left( uint16_t posttion ){ad5696->SetVoltage(Y_LEFT_CHANNEL, ZERO_POS + posttion);}
-  void Y_right( uint16_t posttion){ad5696->SetVoltage(Y_RIGHT_CHANNEL, ZERO_POS + posttion);}
+  void Y_left( uint16_t posttion ){ad5696->SetVoltage(Y_LEFT_CHANNEL,   posttion);}
+  void Y_right( uint16_t posttion){ad5696->SetVoltage(Y_RIGHT_CHANNEL,   posttion);}
     
 private:
-  // DAC_AD5696* ad5696;
-
-  //ES TOdO : get the right numbers
   const uint16_t X_LEFT_CHANNEL = 1;
   const uint16_t Y_LEFT_CHANNEL = 2;
   const uint16_t X_RIGHT_CHANNEL = 4;
   const uint16_t Y_RIGHT_CHANNEL = 8;
   
-  const uint16_t ZERO_POS = 65536/2;
 
 };
 
@@ -141,19 +181,70 @@ public:
     Serial.println(z_m);
 
   }
-  void move(int16_t x, int16_t y, int16_t z)
+  XYZ_t move(int16_t x, int16_t y, int16_t z)
   {
-    x_m += x;
-    y_m += y;
-    z_m += z;
 
+    if ((int64_t) x + x_m > 32000 ) {
+      x_m = 32000;
+      Serial.println("X was restricted to 32000");
+    } else if ((int64_t) x + x_m < -32000 ) {
+      Serial.println("X was restricted to -32000");
+      x_m = -32000;
+    } else {
+      x_m += x;
+    }
+
+
+    if ((int64_t) y + y_m > 32000 ) {
+      y_m = 32000;
+      Serial.println("Y was restricted to 32000");
+    } else if ((int64_t) y + y_m < -32000 ) {
+      Serial.println("Y was restricted to -32000");
+      y_m = -32000;
+    }  else {
+    y_m += y;
+    }
+
+
+    if ((int64_t) z + z_m > 32000 ) {
+      z_m = 32000;
+      Serial.println("Z was restricted to 32000");
+    } else if ((int64_t) z + z_m < -32000 ) {
+      Serial.println("Z was restricted to -32000");
+      z_m = -32000;
+    } else {
+      z_m += z;
+    }
+
+
+   
+   
     dac->X_left( x_m + z_m );
     dac->X_right( -x_m + z_m );
     
     dac->Y_left( y_m + z_m );
     dac->Y_right( -y_m + z_m );
+
+    XYZ_t xyz ;
+    xyz.x = x_m;
+    xyz.y = y_m;
+    xyz.z = z_m;
+
+    return xyz;
     
 
+  }
+
+  void reset()
+  {
+    x_m = y_m = z_m = 0 ;
+
+    dac->X_left( 0 );
+    dac->X_right( 0 );
+    
+    dac->Y_left( 0 );
+    dac->Y_right( 0 );
+    
   }
 
 private:
@@ -166,14 +257,15 @@ private:
 class Scanner
 {
 public:
-  Scanner(){position = new Position(); }// freqs = new FreqSensor();};
+  Scanner(){position = new Position(); freqs = new FreqSensor();};
   void reset(){delete position; position = new Position();}
   void down(uint16_t steps) {Serial.println("Scanner going Down"); position->move(0, 0 , -steps); }//freqs->GetFreq();};
   void up(uint16_t steps) {Serial.println("Scanner going Up");position->move( 0 , 0, steps); }//freqs->GetFreq();};
   void swing(uint16_t steps) {
     Serial.println("Scanner Swing");
 
-    for (int i = 0 ; i < 3 ; i++){
+    for (int i = 0 ; i < 3 ; i++)
+    {
       for (int j =0  ; j < steps ; j+= steps/100)
       {
         position->move(0, 0 , (steps)/100);// * steps/100);
@@ -201,11 +293,46 @@ public:
       Serial.println("Round done !");
     }
   }; // actually we need something more tricky here...
+  void GetFreq()
+  {
+    //  while (1)
+       freqs->GetFreq(); 
+  }
+
+  void GetFreqRange()
+  {
+      freqs->GetFreqRange();
+  }
+
+  void ReadLine()
+  {
+    
+  }
+
+  void land(uint16_t steps)
+  {
+      XYZ_t xyz;
+      // while (freqs->GetFreq().result < THRESHOLD) { 
+      for (int i =0 ; i < 100 & freqs->GetFreq().result < THRESHOLD; i++ )   {
+          xyz = position->move(0, 0 , steps);
+          Serial.print("Landing x: ");
+          Serial.print(xyz.x);
+          Serial.print(" y: ");
+          Serial.print(xyz.y);
+          Serial.print(" z: ");
+          Serial.println(xyz.z);
+
+
+      } 
+
+
+  }
 
 
 private:
   Position* position;
-  // FreqSensor* freqs;
+  FreqSensor* freqs;
+  const uint16_t THRESHOLD = 600;
 
 };
 Scanner* scanner = new Scanner();
@@ -220,8 +347,8 @@ void setup()
 
  
   	ad5696->Init(16, 1, 1);
-  //  unsigned char i2csetup = ADDAC::Setup(LDAC);
-	//   Serial.println(i2csetup == 1 ? "success!" : "failed!");
+   unsigned char i2csetup = ADDAC::Setup(LDAC);
+	  Serial.println(i2csetup == 1 ? "success!" : "failed!");
 
  
 	  // turn internal reference off
@@ -278,13 +405,28 @@ void loop()
 
 //////////////  Freq. sensor functions ////////////////////////////////////
     // scan for operation frequency step of 1-2Hz
+    else if (cmd == "range")
+    {
+      scanner->GetFreqRange();
+    }
 
     // Scan for exact frequency steps 0.1Hz
+    else if (cmd == "freq")
+    {
+      scanner->GetFreq();
+    }
 
 
     // if command scaninit X steps
     // start going down by X steps each time check if frequency has changed
     // if freq. changed stop.
-    
+
+    else if (CheckSingleParameter(cmd, "land", idx, boolean, "land failed"))
+    {
+      Serial.print("Start to softly land with steps of ");
+      Serial.println(idx);
+      scanner->land(idx);
+
+    }
 
 }
